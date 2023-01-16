6996c017359f143c9df6d5c0ba4ba18b49c43e90
Thor Whalen <thorwhalen1@gmail.com>
2023-01-16 16:37:23 +0100

    0.0.2:

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='py2api',
-      version='0.0.1',
-      description='Declarative API entry point',
-      url='http://https://github.com/thorwhalen/py2api.git',
-      author='OtoSense',
-      license='Apache',
-      packages=find_packages(),
-      install_requires=[]
-      )
+setup()  # Note: Everything should be in the local setup.cfg
